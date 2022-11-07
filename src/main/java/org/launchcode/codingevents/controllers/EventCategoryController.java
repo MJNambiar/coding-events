4f632d5fe035de04e1c2f62c49d5502e292fa170
MJNambiar <marinanambiar@Nikhils-MacBook-Air.local>
2022-11-07 07:33:44 -0600

    partway done exercises

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
@@ -0,0 +1,22 @@
+package org.launchcode.codingevents.controllers;
+
+import org.launchcode.codingevents.data.EventCategoryRepository;
+import org.launchcode.codingevents.models.EventCategory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("eventCategories")
+public class EventCategoryController {
+
+    @Autowired
+    private EventCategoryRepository eventCategoryRepository;
+
+    @GetMapping
+    public String displayAllEvents(){
+        return "eventCategories/index";
+    }
+
+}
